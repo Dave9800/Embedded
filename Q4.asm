ace6ce33bb507a9fdadf53834042c79c3291fa59
Dave9800 <75408516+Dave9800@users.noreply.github.com>
2022-10-20 22:29:45 -0400

    Update and rename main.asm to Q4.asm

--- Q4.asm
+++ Q4.asm
@@ -23,20 +23,20 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ;-------------------------------------------------------------------------------
 ; Main loop here
 ;-------------------------------------------------------------------------------
-	        mov.w   #0006h,&2000h;
-            mov.w   #0009h,&2002h;
-            clr.w   &2006h;
-            clr.w   &2008h;
-            mov.w   &2002h,&2004h;
-            add.w   &2000h,&2004h;
-            cmp.w   #000Ah,&2004h;
-            jhs Greater
-            jlo Less
+	    mov.w   #0006h,&2000h;moves 6 to 2000h memory
+            mov.w   #0009h,&2002h;moves 9 to 2002h memory
+            clr.w   &2006h; clears 2006h memory
+            clr.w   &2008h;clears 2008h memory
+            mov.w   &2002h,&2004h; moves whatever is in 2002h to 2004h memory
+            add.w   &2000h,&2004h; adds whatever is in 2000h to 2004h memory
+            cmp.w   #000Ah,&2004h; comparing Ah to whatever is on 2004h memory
+            jhs Greater ; checks if 2004h is greater than Ah
+            jlo Less ; checks if 2004h is less that Ah
 Greater:
-            mov.w   &2000h,&2008h;
+            mov.w   &2000h,&2008h; if it is greater, the output will be included in 2008h
             jmp $;
 Less:
-            mov.w   &2002h,&2006h;
+            mov.w   &2002h,&2006h; if less, the output will be included in 2006h
             jmp $;
                                             
 
