69133f8564e391876d4bc1ec900b4798132be656
Dave9800 <75408516+Dave9800@users.noreply.github.com>
2022-10-20 21:49:59 -0400

    Update Q1.c

--- Q1.c
+++ Q1.c
@@ -1,32 +1,25 @@
-#include <msp430.h>
+#include <msp430.h> 
+
+float b = 5.0;
+/**
+ * main.c
+ */
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+
+    unsigned char c= 0x0f;
+    unsigned char d= 0x5;
+    unsigned char e= c+1;
+    unsigned char f= d-1;
 
 
-float global_float = 6.5;
+    int a = 3;
+    a+=1;
+    b-=1;
 
-int main(){
-
- //(a)
-    printf("Option 'a' solution :-\n\n");
-    int local_int = 5;
-    printf("Address of %d is %p\n",local_int, &local_int);
-    ++local_int;
-    printf("Address of %d after increment is %p\n",local_int, &local_int);
-    printf("\n\n\n");
-    printf("Option 'b' solution :-\n\n");
-    printf("Address of %f is %p\n",global_float, &global_float);
-    --global_float;
-    printf("Address of %f after increment is %p\n",global_float, &global_float);
-    printf("\n\n\n");
-    printf("Option 'c' solution :-\n\n");
-    unsigned char first=0x41;
-    printf("First unsigned char with hex vlaue %x is %c and its address is %x\n",first, first, &first);
-    unsigned char second=0x42;
-    printf("Second unsigned char with hex vlaue %x is %c and its address is %x\n",second, second, &second);
-    unsigned char operation1 = first+1;
-    printf("After first operation hex value is %x and char is %c\n", operation1, operation1);
-    unsigned char operation2 = second-1;
-    printf("After second operation hex value is %x and char is %c\n", operation2, operation2);
-    printf("\n\n\n");
+    e || f;
+    c && d;
 
     return 0;
 }
